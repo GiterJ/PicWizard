202afaf152141fd2116712caec9401dd9e6efbb8
zhu-helin <1197238416@qq.com>
2024-06-26 14:46:35 +0800

    实现了密码加密

--- backend/Project/src/main/java/com/hit/project/entity/User.java
+++ backend/Project/src/main/java/com/hit/project/entity/User.java
@@ -43,4 +43,5 @@ public class User implements Serializable {
     public void setPassword(String password) {
         this.password = password;
     }
+
 }
--- backend/Project/src/main/java/com/hit/project/service/impl/UserServiceImpl.java
+++ backend/Project/src/main/java/com/hit/project/service/impl/UserServiceImpl.java
@@ -6,17 +6,22 @@ import com.hit.project.utils.JSONUtil;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
+
+import static com.hit.project.utils.EncodeUtil.encryptPassword;
 
 @Service
 public class UserServiceImpl implements UserService {
     @Resource
     private UserDao userDao;
+
     @Override
     public JSONUtil login(String name,String password){
         List<User> users = userDao.selectAll();
         for (User user : users) {
-            if (user.getName().equals(name) && user.getPassword().equals(password)) {
+            if (user.getName().equals(name) && user.getPassword().equals(encryptPassword(password))) {
                 System.out.println("[INFO]:登录成功");
                 return new JSONUtil(200, "登陆成功");
             }
@@ -35,7 +40,8 @@ public class UserServiceImpl implements UserService {
                 return new JSONUtil(-1, "用户名重复");
             }
         }
-        userDao.insert(new User(name,password));
+        String encryptedPassword = encryptPassword(password);
+        userDao.insert(new User(name,encryptedPassword));
         System.out.println("[INFO]:注册成功");
         return new JSONUtil(200,"注册成功");
     }
